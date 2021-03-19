4f466572499a1e317d6ec77d5b756c2710d9d311
IsaacWillian <isaacwillian3@gmail.com>
2021-03-19 02:40:14 -0300

    Urls Download and initial scraping

---
+++ DownloadUrls.py
@@ -0,0 +1,6 @@
+import gdown
+
+url = 'https://docs.google.com/spreadsheets/d/1I3yM8ZCn7wvLXTuNUvNYmUJQINf9HbRhTeqqM9QMqoE/export?format=csv&gid=784619377'
+output = 'urls.csv'
+gdown.download(url,output, quiet=False)
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # BirdieChallenge
+
+### This repository contains the solution for Tecnical Challenge for Data Engineer Positon at Birdie
